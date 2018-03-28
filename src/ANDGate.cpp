a4bc0b18ea77dff64f7362f7122ba9a07ad0c728
davidq2010 <davidq2010@gmail.com>
2018-03-27 23:10:22 -0400

    ANDGate added to project

---
+++ src/ANDGate.cpp
@@ -0,0 +1,49 @@
+#ifndef AND_CPP_
+#define AND_CPP_
+
+#include "ANDGate.h"
+
+ANDGate::
+ANDGate() : ProcessorComponent(NUM_INPUTS, NUM_OUTPUTS) {}
+
+
+void
+ANDGate::
+setInput(int _line_id, bool _bit)
+{
+	m_inputs[_line_id] = _bit;
+	m_updated_inputs[_line_id] = true;
+	if (allInputsUpdated())
+		updateOutput();
+}
+
+
+bool
+ANDGate::
+getOutput(int _line_id)
+{
+	return m_outputs[_line_id];
+}
+
+
+void
+ANDGate::
+updateOutput()
+{
+  m_outputs[0] = m_inputs[0] & m_inputs[1];
+
+	fireAllOutputs();
+}
+
+bool
+ANDGate::
+allInputsUpdated()
+{
+	for (int i = 0; i < m_num_inputs; i++)
+		if (!m_updated_inputs[i])
+			return false;
+	return true;
+}
+
+
+#endif // ANDGate_CPP_
