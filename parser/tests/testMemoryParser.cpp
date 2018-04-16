1113d00383b3e3e9a0342c7f3cec093851673bef
lmtuan <lmtuan98@gmail.com>
2018-04-16 09:20:27 -0400

    Add test MemoryParser

---
+++ parser/tests/testMemoryParser.cpp
@@ -0,0 +1,24 @@
+#include "MemoryFileParser"
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+  if (argc != 2) 
+  {
+    cerr << "Usage: " << argv[0] << " [memory_file_name]" << endl;
+    return 0;
+  }
+
+  MemoryFileParser parser;
+  struct MemoryData data = parser(argv[1]);
+  cout << "Starting address:" << hex << showbase << data.start_address << endl << endl;
+  for (auto i : data.data) 
+  {
+    cout << i << endl;
+  }
+
+  return 0;
+}
