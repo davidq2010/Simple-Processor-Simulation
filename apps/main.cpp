cc474030f7549ceb7e7f45a1c66978f4f9c46bfc
lmtuan <lmtuan98@gmail.com>
2018-04-17 02:34:52 -0400

    Main not compiled

--- apps/main.cpp
+++ apps/main.cpp
@@ -3,6 +3,8 @@
 #include "MemoryFileParser.h"
 
 #include "Processor.h"
+
+#include <bitset>
 #include <iostream>
 
 using namespace std;
@@ -16,12 +18,40 @@ int main(int argc, char const *argv[])
 
   //----------------------------------------------------------------------------
   // Parsing input files
+
+  // Read config input file
   ConfigParser config_parser;
   struct Configs configs = config_parser(argv[1]);
 
-  MemoryFileParser mem_parser;
-  struct MemoryData mem_data = mem_parser(configs.)
+  // Assemble the program
+  ASMParser assembler;
+  vector<Instruction> instructions = assembler(configs.program_input);
+  vector<unsigned long> binary_code;
+  binary_code.reserve(instructions.length());
+  for (auto& i : instructions)
+    binary_code.push_back( bitset<32>(i.getEncoding()).to_ulong() );
 
+  // read memory input file
+  MemoryFileParser mem_parser;
+  struct MemoryContent mem_content = mem_parser(configs.memory_contents_input);
+
+  // read register input file
+  struct MemoryContent reg_content = mem_parser(configs.register_file_input);
+
+  //----------------------------------------------------------------------------
+  // Init processor
+  Processor processor(binary_code, reg_content.data, 
+                      mem_content.data, mem_content.start_address);
+
+  Logger logger;
+  processor.setLogger(&logger);
+
+  //----------------------------------------------------------------------------
+  // Run
+
+  while (true) {
+    processor.step();
+  }
 
   return 0;
 }
